e138430a9f8af7ce5c3ccb67851d6d9ddac30221
NguyenVanHuy1405 <vanhuy141520@gmail.com>
2023-06-29 22:46:49 +0700

    Build default layout frame

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import styles from './header.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 const index = () => {
-    return (
-        <div>
-            <h2>Header</h2>
+    return <header className={cx('wrapper')}>
+        <div className={cx('content')}>
+
         </div>
-    )
+    </header>
 }
 
 export default index
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,18 @@
 import { Children } from 'react';
 import Header from './Header'
 import SideBar from './SideBar'
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({children}) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <SideBar />
-                <div className="content">
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
