a87a8cf662dc7082922790d9b21d30f701cbfcad
NguyenVanHuy1405 <vanhuy141520@gmail.com>
2023-08-06 14:41:03 +0700

    Build sidebar for project part1

--- src/layouts/DefaultLayout/SideBar/index.js
+++ src/layouts/DefaultLayout/SideBar/index.js
@@ -3,14 +3,16 @@ import styles from './sideBar.module.scss'
 import classNames from 'classnames/bind'
 import config from '~/config'
 import Menu, { MenuItem } from '~/layouts/DefaultLayout/SideBar/menu';
+import { HomeIcon, UserGroupIcon, LiveIcon, LiveActiveIcon, UserGroupActiveIcon, HomeActiveIcon } from '~/components/Icons';
+
 const cx = classNames.bind(styles)
 
 const index = () => {
   return <aside className={cx('wrapper')}>
     <Menu>
-      <MenuItem title="For you" to={config.routes.home} icon={null}/>
-      <MenuItem title="For you" to={config.routes.home} icon={null}/>
-      <MenuItem title="For you" to={config.routes.home} icon={null}/>
+      <MenuItem title="For you" to={config.routes.home} icon={<HomeIcon />} activeIcon= {<HomeActiveIcon />}/>
+      <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon />} activeIcon= {<UserGroupActiveIcon/>}/>
+      <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon= {<LiveActiveIcon />}/>
     </Menu>
   </aside>
 }
--- src/layouts/DefaultLayout/SideBar/menu/MenuItem.js
+++ src/layouts/DefaultLayout/SideBar/menu/MenuItem.js
@@ -4,11 +4,12 @@ import { NavLink } from 'react-router-dom';
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
-function MenuItem({ title, icon, to}) {
+function MenuItem({ title, icon, to, activeIcon}) {
     return ( 
-        <NavLink to={to}>
-            {icon}
-            <span>{title}</span>
+        <NavLink to={to} className={(nav) =>cx('menu-item', {active:nav.isActive})}>
+            <span className={cx('icon')}>{icon}</span>
+            <span className={cx('active-icon')}>{activeIcon}</span>
+            <span className={cx('title')}>{title}</span>
         </NavLink>
     );
 }
